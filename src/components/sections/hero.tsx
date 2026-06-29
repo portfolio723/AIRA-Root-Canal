94a4af6a82f4855174ec6b1a0091a92746c5f61e
Sandeep M <723msfree@gmail.com>
2026-06-29 04:15:45 +0000

    remove the background images or icons in the bg of the hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -47,12 +47,6 @@ export function Hero() {
         
         {/* LEFT SECTION (55%) */}
         <div className="w-full lg:w-[55%] relative px-4 md:px-8 lg:px-12 pb-12 flex flex-col justify-center">
-          {/* Subtle Background Patterns */}
-          <div className="absolute inset-0 z-0 opacity-[0.05] pointer-events-none">
-            <div className="absolute top-20 left-20 w-64 h-64 border-[1px] border-primary rounded-full" />
-            <div className="absolute bottom-40 right-40 w-96 h-96 border-[1px] border-primary rounded-full" />
-          </div>
-
           {/* Telugu Regional Tag */}
           <div className="relative z-10 mb-4 animate-in fade-in duration-1000">
             <h2 className="text-3xl md:text-5xl lg:text-7xl font-bold text-primary/10 select-none pointer-events-none font-headline">
